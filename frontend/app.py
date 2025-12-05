893acdb8bfe24ce2e00304226e33787ba2b7b110
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-12-05 23:23:57 +0100

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -9,29 +9,29 @@ FRONTEND_DIR = os.path.dirname(os.path.abspath(__file__))
 SCRIPTS_DIR = os.path.join(FRONTEND_DIR, "..", "scripts")
 SUMMARY_DIR = os.path.join(FRONTEND_DIR, "..", "summary")
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    domains = sorted([d for d in os.listdir(SUMMARY_DIR) if os.path.isdir(os.path.join(SUMMARY_DIR, d))])
-    selected_domain = None
+    total_file = os.path.join(SUMMARY_DIR, "total.json")
+    with open(total_file, "r") as f:
+        summary = json.load(f)
+    return render_template("index.html", summary=summary)
+
+@app.route("/domain/<domain>")
+def domain_page(domain):
+    day = request.args.get("day")  # YYYY-MM-DD
+    domain_script = os.path.join(SCRIPTS_DIR, "domain.py")
+    subprocess.Popen(["python3", domain_script, domain])
+    summary_file = os.path.join(SUMMARY_DIR, domain, "summary.json")
     domain_summary = {}
     day_data = {}
+    if os.path.isfile(summary_file):
+        with open(summary_file) as f:
+            domain_summary = json.load(f)
+        if day and day in domain_summary:
+            day_data = domain_summary[day]
 
-    if request.method == "POST":
-        selected_domain = request.form.get("domain")
-        day = request.form.get("day")  #  YYYY-MM-DD
-        domain_script = os.path.join(SCRIPTS_DIR, "domain.py")
-        subprocess.Popen(["python3", domain_script, selected_domain])
-        summary_file = os.path.join(SUMMARY_DIR, selected_domain, "summary.json")
-
-        if os.path.isfile(summary_file):
-            with open(summary_file) as f:
-                domain_summary = json.load(f)
-            if day in domain_summary:
-                day_data = domain_summary[day]
-
-    return render_template("index.html",
-                           domains=domains,
-                           selected_domain=selected_domain,
+    return render_template("domain.html",
+                           selected_domain=domain,
                            domain_summary=domain_summary,
                            day_data=day_data)
 
