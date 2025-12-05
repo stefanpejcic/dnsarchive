5b70cc75b7942532482343275b23806a9ca2e27c
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-12-06 00:29:27 +0100

    Update domain.py

--- backend/domain.py
+++ backend/domain.py
@@ -23,7 +23,7 @@ if os.path.exists(OUTFILE):
     if datetime.now() - file_mtime < timedelta(hours=24):
         with open(OUTFILE, "r") as f:
             existing_content = json.load(f)
-        existing_content["message"] = f"Domain {DOMAIN} was already scanned in the last 24h. Showing existing file content."
+        existing_content["message"] = f"Domain {DOMAIN} was already scanned in the last 24h."
 
         print(json.dumps(existing_content, indent=2))
         sys.exit(0)
